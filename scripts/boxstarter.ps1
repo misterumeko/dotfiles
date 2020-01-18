2acff489f4d41c6ff12d2a1d311b3077b8ac3cf3
Mister Umeko <43111030+misterumeko@users.noreply.github.com>
2020-01-18 15:28:54 +0900

    Edit Boxstarter script

--- scripts/boxstarter.ps1
+++ scripts/boxstarter.ps1
@@ -4,34 +4,34 @@
 #
 # Run (in Boxstarter Shell):
 # Install-BoxstarterPackage -PackageName <THIS_URL>
+#
+# Chocolateyで別途インストール:
+# choco install qttabbar # インストール時にエラーダイアログが出るため。
+# choco install visualstudio2019community # Boxstarterだと失敗するため。PowerShellならOK。
 
 Disable-UAC
 Disable-BingSearch
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowFileExtensions -DisableShowRecentFilesInQuickAccess -DisableShowFrequentFoldersInQuickAccess
+Set-TaskbarOptions -Size Small
 
 choco install Microsoft-Hyper-V-All -source windowsFeatures
 choco install Microsoft-Windows-Subsystem-Linux -source windowsfeatures
 
 choco install vscode
+choco install microsoft-windows-terminal
 choco install googlechrome
 choco install firefox
-choco install docker-for-windows
 choco install 7zip.install
 choco install git.install
-# choco install sourcetree # Chocolatey経由だとスタートメニューに表示されない。
-choco install hyper
+choco install sourcetree
 choco install keyhac
 choco install adobereader
-# choco install visualstudio2017community # Boxstarter経由だと失敗する (PowerShellだとOK)。
 choco install vlc
-choco install anaconda3
+choco install miniconda3
 choco install nvm
-
-nvm install 8.12.0
-nvm use 8.12.0
-
-Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu-1604 -OutFile ~/Ubuntu.appx -UseBasicParsing
-Add-AppxPackage -Path ~/Ubuntu.appx
+choco install kindle
+choco install strokesplus.install
+choco install docker-desktop # 再起動の度にDockerが起動するから最後にインストールする。
 
 Enable-UAC
 Enable-MicrosoftUpdate
